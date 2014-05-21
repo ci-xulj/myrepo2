0d4ab8f648597314ede1e42b544c673b1f12be38
kfzx-xulj <kfzx-xulj@10.84.152.167>
2014-05-21 10:53:03 +0800

    a

---
+++ src/main/java/testTravis/HiTravis.java
@@ -0,0 +1,8 @@
+package testTravis;
+
+public class HiTravis {
+
+	public static void sayHi(){
+		System.out.println("Hi, travis");
+	}
+}
---
+++ src/test/java/testTravis/TestTravis.java
@@ -0,0 +1,11 @@
+package testTravis;
+
+import org.junit.Test;
+
+public class TestTravis {
+
+	@Test
+	public void testHi(){
+		HiTravis.sayHi();
+	}
+}
